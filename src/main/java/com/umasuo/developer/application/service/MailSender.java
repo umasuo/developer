a1e346559927149790209b971f55df4ca04203c1
davis999 <davis.dai@go6d.com>
2017-07-11 16:37:22 +0800

    fix mail sender bug

--- src/main/java/com/umasuo/developer/application/service/MailSender.java
+++ src/main/java/com/umasuo/developer/application/service/MailSender.java
@@ -8,12 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by Davis on 17/7/10.
  */
-@Component
+@Service(value = "developerMailSender")
 public class MailSender {
 
   /**
--- src/main/java/com/umasuo/developer/application/service/VerificationApplication.java
+++ src/main/java/com/umasuo/developer/application/service/VerificationApplication.java
@@ -12,6 +12,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
